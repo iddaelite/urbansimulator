2e2aabf794b584c33632792a2e870469de597d8c
Ben Coorey <info@bencoorey.com>
2017-05-06 13:27:43 +1000

    Setup Staging for Project

--- Urban Simulator/UrbanSimulatorCommand.cs
+++ Urban Simulator/UrbanSimulatorCommand.cs
@@ -27,48 +27,21 @@ namespace Urban_Simulator
         ///<returns>The command name as it appears on the Rhino command line.</returns>
         public override string EnglishName
         {
-            get { return "UrbanSimulatorCommand"; }
+            get { return "UrbanSimulator"; }
         }
 
         protected override Result RunCommand(RhinoDoc doc, RunMode mode)
         {
-            // TODO: start here modifying the behaviour of your command.
-            // ---
-            RhinoApp.WriteLine("The {0} command will add a line right now.", EnglishName);
 
-            Point3d pt0;
+            RhinoApp.WriteLine("The Urban Simulator has begun.");
 
-            using (GetPoint getPointAction = new GetPoint())
-            {
-                getPointAction.SetCommandPrompt("Please select the start point");
-                if (getPointAction.Get() != GetResult.Point)
-                {
-                    RhinoApp.WriteLine("No start point was selected.");
-                    return getPointAction.CommandResult();
-                }
-                pt0 = getPointAction.Point();
-            }
+            //getPrecinct()                 //Ask user to select a surface representing the Precinct
+            //generateRoadNetwork()         //Using the precinct, Generate a Road Network
+            //createBlocks()                //Using the road network, create blocks
+            //subdivideBlocks()             //Subdivide the blocks into Plots
+            //instantiateBuildings()        //Place buildings on each plot
 
-            Point3d pt1;
-            using (GetPoint getPointAction = new GetPoint())
-            {
-                getPointAction.SetCommandPrompt("Please select the end point");
-                getPointAction.SetBasePoint(pt0, true);
-                getPointAction.DynamicDraw +=
-                  (sender, e) => e.Display.DrawLine(pt0, e.CurrentPoint, System.Drawing.Color.DarkRed);
-                if (getPointAction.Get() != GetResult.Point)
-                {
-                    RhinoApp.WriteLine("No end point was selected.");
-                    return getPointAction.CommandResult();
-                }
-                pt1 = getPointAction.Point();
-            }
-
-            doc.Objects.AddLine(pt0, pt1);
-            doc.Views.Redraw();
-            RhinoApp.WriteLine("The {0} command added one line to the document.", EnglishName);
-
-            // ---
+            RhinoApp.WriteLine("The Urban Simulator is complete.");
 
             return Result.Success;
         }
